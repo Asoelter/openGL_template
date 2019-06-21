330d4f4b412fefcffe7dfa2ba571d32188e7652d
Adam Soelter <adamdlsoelter@gmail.com>
2019-06-20 21:15:38 -0400

    first commit

---
+++ rogue/graphics/vertex_buffer.cpp
@@ -0,0 +1,13 @@
+#include "vertex_buffer.h"
+
+#include <iostream>
+
+VertexBuffer::VertexBuffer()
+{
+	std::cout << "Vertex Buffer Constructed\n";
+}
+
+VertexBuffer::~VertexBuffer()
+{
+	std::cout << "Vertex Buffer Destroyed\n";
+}
---
+++ rogue/graphics/vertex_buffer.h
@@ -0,0 +1,15 @@
+#ifndef VERTEX_BUFFER_H
+#define VERTEX_BUFFER_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+class VertexBuffer
+{
+public:
+	VertexBuffer();
+	~VertexBuffer();
+private:
+};
+
+#endif //VERTEX_BUFFER_H
